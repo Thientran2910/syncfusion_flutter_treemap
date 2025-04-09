2c55b390dd35f2ee5af0995b397b303a52bac233
thientv <thientran2910@gmail.com>
2025-04-09 15:38:44 +0700

    feat: upgrade

--- lib/treemap.dart
+++ lib/treemap.dart
@@ -3049,7 +3049,10 @@ class SfTreemap extends StatelessWidget {
       tileHoverBorder: tileHoverBorder ??
           RoundedRectangleBorder(
             side: BorderSide(
-              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+              color: Theme.of(context)
+                  .colorScheme
+                  .onSurface
+                  .withValues(alpha: 0.5),
             ),
           ),
       onSelectionChanged: onSelectionChanged,
